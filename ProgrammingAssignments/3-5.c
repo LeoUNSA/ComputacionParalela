556f2dca57a8fd32ef67366523bf2e60ba6cdf07
LeoUNSA <104027221+LeoUNSA@users.noreply.github.com>
2024-10-28 10:21:21 -0500

    Add files via upload

--- ProgrammingAssignments/3-5.c
+++ ProgrammingAssignments/3-5.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+// Global variables for MPI
+int comm_sz;
+int my_rank;
 void Read_matrix(char* prompt, double local_A[], int local_n, int n, int my_rank);
 void Read_vector(char* prompt, double x[], int n, int my_rank);
 void Print_matrix(char* title, double local_A[], int local_n, int n, int my_rank);
 void Print_vector(char* title, double y[], double local_y[], int n, int local_n, int my_rank);
 int main(void) {
-    int my_rank, comm_sz;
     double* local_A = NULL;
     double* local_x = NULL;
     double* local_y = NULL;
@@ -18,11 +20,9 @@ int main(void) {
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
     // Process 0 reads matrix size
     if (my_rank == 0) {
         printf("Enter matrix order n: ");
-        fflush(stdout);
         scanf("%d", &n);
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -37,18 +37,15 @@ int main(void) {
     }
     // Read and distribute matrix
     Read_matrix("Enter the matrix", local_A, local_n, n, my_rank);
-    fflush(stdout);
     // Read and distribute vector
     if (my_rank == 0) {
         printf("Enter the vector:\n");
-        fflush(stdout);
         for (int i = 0; i < n; i++)
             scanf("%lf", &x[i]);
     }
     // Scatter vector x to all processes
     MPI_Scatter(x, local_n, MPI_DOUBLE, local_x, local_n, 
                 MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
     // Compute local matrix-vector product
     for (int i = 0; i < n; i++) {
         local_y[i] = 0.0;
@@ -56,7 +53,14 @@ int main(void) {
             local_y[i] += local_A[i*local_n + j] * local_x[j];
     }
     // Sum up partial results using MPI_Reduce_scatter
-    MPI_Reduce_scatter(local_y, y, &local_n, MPI_DOUBLE, 
+    int* recvcounts = malloc(comm_sz * sizeof(int));
+    for (int i = 0; i < comm_sz; i++) {
+        recvcounts[i] = local_n;
+    }
+    if (my_rank == 0) {
+        y = malloc(n * sizeof(double));
+    }
+    MPI_Reduce_scatter(local_y, y, recvcounts, MPI_DOUBLE, 
                       MPI_SUM, MPI_COMM_WORLD);
     // Print results
     Print_vector("The product is", y, local_y, n, local_n, my_rank);
@@ -64,27 +68,30 @@ int main(void) {
     free(local_A);
     free(local_x);
     free(local_y);
+    free(recvcounts);
     if (my_rank == 0) {
         free(x);
         free(y);
     }
+
     MPI_Finalize();
     return 0;
 }
 void Read_matrix(char* prompt, double local_A[], int local_n, int n, int my_rank) {
     double* A = NULL;
     int local_ok = 1;
-
     if (my_rank == 0) {
         A = malloc(n * n * sizeof(double));
         printf("%s\n", prompt);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 scanf("%lf", &A[i*n + j]);
+
         // Distribute matrix by columns to all processes
         for (int i = 0; i < n; i++)
             for (int j = 0; j < local_n; j++)
                 local_A[i*local_n + j] = A[i*n + j];
+
         for (int dest = 1; dest < comm_sz; dest++) {
             int start_col = dest * local_n;
             for (int i = 0; i < n; i++)
